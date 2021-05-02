e424ae24db92d222e10fccf47a0cc9a619434947
Umberto M <Umberto.messuti@mail.polimi.it>
2021-05-02 09:59:04 +0200

    Imported TopBar and LowBar from HTML Templates

--- Response/components/LowBar.vue
+++ Response/components/LowBar.vue
@@ -1,13 +1,179 @@
 <template>
-  <div class="LowBar">
-    
+  <div ref="doc" class="lowBar">
+    <ul id="bread-crumbs">
+      <li>
+        <span class="text-theme-color">/</span>&nbsp;<a href="#">Area</a>&nbsp;
+      </li>
+      <li>
+        <span class="text-theme-color">/</span>&nbsp;<a href="#">AI</a>&nbsp;
+      </li>
+      <li>
+        <span class="text-theme-color">/</span>&nbsp;<a href="#">Nome Bello</a
+        >&nbsp;
+      </li>
+    </ul>
+    <ul id="chapters" ref="chapters" class="navbar-invisible">
+      <li>
+        <a href="#">Overview</a>
+      </li>
+      <li>
+        <a href="#">Services</a>
+      </li>
+      <li>
+        <a href="#">Team</a>
+      </li>
+      <li>
+        <a href="#">Case Studies</a>
+      </li>
+    </ul>
+    <div class="capBurger" @click="navSlide">
+      <p>Cap.&nbsp;</p>
+      <p id="arrow" ref="arrow" class="text-theme-color">&lt;</p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'LowBar',
+  data() {
+    return {
+      arrow: Object,
+      capBurger: Object,
+      chapters: Object,
+      capLinks: Object,
+    }
+  },
+  mounted() {
+    this.arrow = this.$refs.arrow
+    this.capBurger = this.$refs.doc.querySelector('.capBurger')
+    this.chapters = this.$refs.chapters
+    this.capLinks = this.$refs.doc.querySelectorAll('#chapters > li')
+  },
+  methods: {
+    navSlide() {
+      // Show or Hide Navbar links
+      this.chapters.classList.toggle('navbar-invisible')
+      // Burger Animation
+      this.arrow.classList.toggle('down-arrow')
+
+      // Link fade animation
+      this.capLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          setTimeout(() => {
+            link.style.animation = ''
+          }, 300)
+        } else {
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s`
+        }
+      })
+    },
+  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.lowBar {
+  opacity: 0.95;
+  padding: 0 10px;
+  background-color: #bbb;
+  position: fixed;
+  left: 0;
+  top: 6vh;
+  z-index: 9;
+  height: 3vh;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.lowBar * {
+  transition-duration: 300ms;
+}
+
+.lowBar ul {
+  display: flex;
+  flex-direction: row;
+}
+
+#chapters li {
+  padding: 0 10px;
+}
+
+.lowBar a {
+  color: #333;
+}
+
+.lowBar a:hover {
+  letter-spacing: 1px;
+  padding: 0px 1px;
+  color: #fff;
+}
+
+.lowBar .capBurger {
+  position: absolute;
+  right: 0px;
+  display: none;
+  width: 80px;
+}
+
+#arrow {
+  font-weight: 600;
+  font-size: 2rem;
+  color: #ec7404;
+  letter-spacing: 1px;
+}
+
+@media (max-width: 750px) {
+  #chapters {
+    position: absolute;
+    top: 3vh;
+    left: 0px;
+    font-size: 3rem;
+    height: 91vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    background-color: #ccc;
+    transition: 500ms;
+  }
+
+  #chapters li {
+    opacity: 0;
+    padding: 20px;
+  }
+
+  .lowBar .capBurger {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .navbar-invisible {
+    transform: translateX(100%);
+  }
+
+  .down-arrow {
+    transform: rotate(-90deg);
+  }
+}
+</style>
+
+<style>
+/* Animations */
+@keyframes navLinkFade {
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
+}
+</style>
--- Response/components/TopBar.vue
+++ Response/components/TopBar.vue
@@ -1,32 +1,88 @@
 <template>
-  <div id="top-bar">
-    <div id="logo">
+  <div id="navbar" ref="doc" class="navbar">
+    <div class="logo">
       <h1>
         <a href=""><span class="text-primary">R</span>esponse</a>
       </h1>
     </div>
-
-    <nav id="nav-bar">
-      <ul id="nav-links">
+    <nav>
+      <ul class="nav-links">
         <li>
-          <a href="#">Home</a>
+          <div class="subsection">
+            <a href="#">About Us</a>
+          </div>
         </li>
         <li>
-          <a href="#">About Us</a>
+          <div class="subsection">
+            <p>Services</p>
+          </div>
+          <ul class="submenu">
+            <li>
+              <a href="#">All Services</a>
+            </li>
+            <li>
+              <a href="#">AI</a>
+            </li>
+            <li>
+              <a href="#">Cyber Security</a>
+            </li>
+            <li>
+              <a href="#">Digital Services</a>
+            </li>
+            <li>
+              <a href="#">Cloud Services</a>
+            </li>
+          </ul>
         </li>
         <li>
-          <a href="#">Services</a>
+          <div class="subsection">
+            <p>Industries</p>
+          </div>
+          <ul class="submenu">
+            <li>
+              <a href="#">All Industries</a>
+            </li>
+            <li>
+              <a href="#">IND 1</a>
+            </li>
+            <li>
+              <a href="#">IND 2</a>
+            </li>
+            <li>
+              <a href="#">IND 3</a>
+            </li>
+          </ul>
         </li>
         <li>
-          <a href="#">Industries</a>
-        </li>
-        <li>
-          <a href="#">Team</a>
-        </li>
-        <li>
-          <a href="#">Contact Us</a>
+          <div class="subsection">
+            <p>Team</p>
+          </div>
+          <ul class="submenu">
+            <li>
+              <a href="#">All Team</a>
+            </li>
+            <li>
+              <a href="#">AI</a>
+            </li>
+            <li>
+              <a href="#">Cyber Security</a>
+            </li>
+            <li>
+              <a href="#">Digital Services</a>
+            </li>
+            <li>
+              <a href="#">Cloud Services</a>
+            </li>
+          </ul>
         </li>
       </ul>
+      <div class="burger-container">
+        <div class="burger" @click="navSlide">
+          <div class="line1"></div>
+          <div class="line2"></div>
+          <div class="line3"></div>
+        </div>
+      </div>
     </nav>
   </div>
 </template>
@@ -34,19 +90,53 @@
 <script>
 export default {
   name: 'TopBar',
+  data() {
+    return {
+      burger: Object,
+      nav: Object,
+      navLinks: Object,
+    }
+  },
+  mounted() {
+    this.burger = this.$refs.doc.querySelector('.burger')
+    this.nav = this.$refs.doc.querySelector('.nav-links')
+    this.navLinks = this.$refs.doc.querySelectorAll('.nav-links > li')
+  },
+  methods: {
+    navSlide() {
+      // Show or Hide Navbar links
+      this.nav.classList.toggle('navbar-visible')
+      // Burger Animation
+      this.burger.classList.toggle('switch')
+
+      // Link fade animation
+      this.navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+          setTimeout(() => {
+            link.style.animation = ''
+          }, 300)
+        } else {
+          // link.style.opacity = 1
+          link.style.animation = `navLinkFade 0.5s ease forwards ${
+            index / 7 + 0.5
+          }s`
+        }
+      })
+    },
+  },
 }
 </script>
 
 <style scoped>
-/*Top Bar*/
-#top-bar {
+/* Navbar */
+.navbar {
   opacity: 0.95;
   position: fixed;
   top: 0;
   width: 100%;
   display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: flex-start;
   text-align: center;
   justify-content: space-between;
   height: 6vh;
@@ -55,49 +145,254 @@ export default {
   z-index: 11;
 }
 
-#top-bar * {
-  transition-duration: 300ms;
+.navbar * {
+  transition: 200ms;
 }
 
-/*Naigation Links*/
-#nav-links {
+/* Logo */
+.navbar .logo a {
+  color: #333;
+}
+
+.navbar .logo a:hover {
+  color: #fff;
+}
+
+.navbar .logo {
+  height: 6vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.navbar .logo > * {
+  display: block;
+}
+
+.navbar .logo h1 {
+  font-size: 1.7rem;
+  letter-spacing: -10px;
+  margin: 0px 15px;
+  font-weight: 500;
+}
+
+.navbar .logo:hover h1 {
+  letter-spacing: 1px;
+}
+
+.navbar .logo h1 .text-primary {
+  font-family: 'Lobster', cursive;
+}
+
+/* Nav Link */
+.nav-links {
   display: flex;
 }
 
-#nav-links li a {
+.nav-links > li {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.nav-links > li a,
+.nav-links > li p {
+  display: block;
   color: #ec7404;
   padding: 1px 10px;
   margin: 0 5px;
 }
 
-#nav-links li a:hover {
-  letter-spacing: 1px;
-  padding: 5px 10px;
-  color: #fff;
-  border-bottom: #fff 2px solid;
+.nav-links .subsection {
+  height: 6vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-#nav-links a.current {
+.nav-links li a:hover {
   color: #fff;
+  /* border-bottom: #fff 2px solid; */
 }
 
-/*Logo*/
-#logo a {
+/* Submenu */
+.nav-links li .submenu {
+  visibility: hidden;
+  opacity: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: auto;
+  background-color: #333;
+}
+
+.nav-links li .submenu li {
+  display: flex;
+  justify-content: center;
+  align-items: center;
   color: #333;
+  height: 0;
 }
 
-#logo a:hover {
-  letter-spacing: 1px;
+.nav-links li:hover .submenu {
+  visibility: visible;
+  opacity: 1;
+}
+
+.nav-links li:hover .submenu li {
+  width: 100%;
+  height: 4vh;
+  border-bottom: 1px #ec7404 solid;
+}
+
+.nav-links li:hover p {
+  color: #ddd;
+}
+
+.nav-links li .submenu a {
+  color: #ec7404;
+}
+
+.nav-links li .submenu a:hover {
+  color: #ddd;
+}
+
+/* Burger Button */
+.burger {
+  display: none;
+}
+
+.burger-container {
+  width: 6vh;
+  height: 6vh;
+  display: none;
+  align-items: center;
+  justify-content: center;
+}
+
+.burger > div {
+  width: 25px;
+  height: 3px;
+  background-color: #ec7404;
+  margin: 5px;
+}
+
+.nav-links a.current {
   color: #fff;
 }
 
-#logo h1 {
-  letter-spacing: -10px;
-  margin: 20px;
-  font-weight: 500;
-}
+/* Mobile */
+@media (max-width: 750px) {
+  /* Navbar */
+  .nav-links {
+    position: absolute;
+    top: 6vh;
+    left: 0px;
+    font-size: 3rem;
+    height: 94vh;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    flex-direction: column;
+    background-color: #333;
+    transform: translateX(100%);
+    transition: 200ms;
+  }
 
-#logo span {
-  font-family: 'Lobster', cursive;
+  .nav-links > li {
+    opacity: 0;
+    padding: 6%;
+  }
+
+  .navbar-visible {
+    transform: translateX(0);
+  }
+
+  .burger {
+    display: block;
+    cursor: pointer;
+    margin: auto;
+  }
+
+  .burger-container {
+    display: flex;
+  }
+
+  /* Submenu */
+  .nav-links > li > .submenu {
+    display: none;
+    visibility: hidden;
+    opacity: 0;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: auto;
+    background-color: #333;
+    transform: translateX(100px);
+  }
+
+  .nav-links > li:hover > .submenu {
+    visibility: visible;
+    display: flex;
+    margin: 20px;
+    opacity: 1;
+    transform: translateX(0px);
+  }
+
+  .nav-links > li > .submenu > li {
+    opacity: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #333;
+    height: 0;
+  }
+
+  .nav-links > li:hover > .submenu > li {
+    transition: all 100ms;
+    opacity: 1;
+    height: 6vh;
+    font-size: 2rem;
+    width: 100%;
+    border-bottom: 1px #ec7404 solid;
+  }
+
+  .nav-links > li:hover > p {
+    color: #ddd;
+  }
+
+  .nav-links > li > .submenu > a {
+    color: #ec7404;
+  }
+
+  .nav-links li .submenu a:hover {
+    color: #ddd;
+  }
+}
+/* Burger Animation */
+.switch .line1 {
+  transform: rotate(-45deg) translate(-5px, 6px);
+}
+.switch .line2 {
+  opacity: 0;
+}
+.switch .line3 {
+  transform: rotate(45deg) translate(-5px, -6px);
+}
+</style>
+
+<style>
+/* Animations */
+@keyframes navLinkFade {
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0px);
+  }
 }
 </style>
